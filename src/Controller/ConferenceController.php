ee8e4efaad63fb4b284fe2546450e27636776520
parchetak <parchetak@outlook.com>
2024-08-25 10:43:22 +0100

    easter egg v2

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name): Response
     {
         $greet = '';
-        if ($name= $request->query->get('hello'))
+        if ($name)
         {
             $greet = sprintf('<h1>Hello %s!</h1>',htmlspecialchars($name));
         }
