b53dc939785ade8cedf7ce3502868dde99fc9823
parchetak <parchetak@outlook.com>
2024-08-25 10:50:25 +0100

    Dump Function

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,32 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
-        $greet = '';
-        if ($name)
-        {
-            $greet = sprintf('<h1>Hello %s!</h1>',htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-            <html> 
-                <body>
-                    $greet
-                    <img src="/images/under-construction.gif"/>
-                </body>
-            </html> 
-            EOF
-        );
-        /*
+        dump($request);
+
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
-        */
     }
 }
